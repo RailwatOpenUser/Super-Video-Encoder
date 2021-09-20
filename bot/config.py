fe7e484f5ed650b5e38d6727c464bb22f4df6370
RailwatOpenUser <90463616+RailwatOpenUser@users.noreply.github.com>
2021-09-20 13:27:45 +0530

    Update config.py

--- bot/config.py
+++ bot/config.py
@@ -16,16 +16,16 @@
 from decouple import config
 
 try:
-    APP_ID = config("APP_ID", cast=int)
-    API_HASH = config("API_HASH")
-    BOT_TOKEN = config("BOT_TOKEN")
+    APP_ID = config("a9e62ec83fe3c22379e3e19195c8b3f6", cast=int)
+    API_HASH = config("3281305")
+    BOT_TOKEN = config("1929972601:AAE9UKpipafRDDA-tgl1HmmL6zQpjw6a7cI")
     DEV = 1322549723
-    OWNER = config("OWNER")
+    OWNER = config("1666551439")
     FFMPEG = config(
-        "FFMPEG",
+        "ffmpeg -i '''{}'''  -filter_complex 'drawtext=fontfile=njnaruto.ttf:fontsize=25:fontcolor=white:bordercolor=black@0.50:x=w-tw-10:y=10:box=1:boxcolor=black@0.5:boxborderw=6:text='Animes-Encoded'':enable='between(t,0,15)':alpha='if(lt(t,14)\\,1\\,if(lt(t\\,15)\\,(1-(t-14))/1\\,0))', drawtext=fontfile=njnaruto.ttf:text='\\|\\| Download From The Original Place https\\://t.me/Animes_Encoded \\|\\| And For Animes Request https\\://t.me/Anime_Hub_Group':bordercolor=black@0.50:borderw=5:fontcolor=white:fontsize=30:x=w-((2*w-200)*(t-615)/60):y=lh+0.5:enable='between(t, 615,680)':alpha='if(lt(t,679)\\,1\\,if(lt(t\\,680)\\,(1-(t-679))/1\\,0))'[out1]'' -metadata:s:a:0 title=''[Telegram: @Animes_Encoded]'' -metadata:s:a:1 title=''[Telegram: @Animes_Encoded]'' -metadata:s:s:0 title=''[Telegram: @Animes_Encoded]~English'' -metadata:s:s:1 title=''[Telegram: @Animes_Encoded]~English'' -map [out1] -map 0:a? -map 0:s? -map 0:t? -metadata title='Animes_Encoded - 480p'' -c:v libx265 -pix_fmt yuv420p -preset medium -s 846x480 -crf 32.8 -c:a libfdk_aac -profile:a aac_he_v2  -ac 2 -vbr 2 -c:s copy -movflags +faststart '''{}'''  -y",
         default='ffmpeg -i "{}" -preset ultrafast -c:v libx265 -crf 27 -map 0:v -c:a aac -map 0:a -c:s copy -map 0:s? "{}"',
     )
-    THUMB = config("THUMBNAIL")
+    THUMB = config("https://telegra.ph/file/269d7d7b5f11635d4c8a0.jpg")
 except Exception as e:
     LOGS.info("Environment vars Missing")
     LOGS.info("something went wrong")
